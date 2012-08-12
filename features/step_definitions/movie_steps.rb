e36e1ecb05b60d3f35c229e72458d2fcbb61a9f2
mrichard <mike_r1978_6@hotmail.com>
2012-08-11 19:06:54 -0700

    homework 3 part 2 done

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,8 +4,9 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create!(movie)
   end
-  flunk "Unimplemented"
+  #flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -17,6 +18,16 @@ Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   flunk "Unimplemented"
 end
 
+Then /I should see (all|none) of the movies/ do |selection|
+  total_movies = page.all("tbody#movielist tr").count
+  puts "Total movies are #{total_movies}"
+  if selection == "all"
+    total_movies.should == 10
+  elsif selection == "none"
+    total_movies.should == 0
+  end
+end
+
 # Make it easier to express checking or unchecking several boxes at once
 #  "When I uncheck the following ratings: PG, G, R"
 #  "When I check the following ratings: G"
@@ -25,4 +36,11 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  
+  uncheck = "" unless uncheck == "un"
+  rating_array = rating_list.split(",")
+  rating_array.each do |rating|
+    rating = rating.strip
+    step %{I #{uncheck}check "ratings_#{rating}"}
+  end
 end
